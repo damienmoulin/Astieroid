77068989c4646d3618b380d4668968a64c6600cd
damienmoulin <damien.moulin1@gmail.com>
2016-12-13 10:16:55 +0100

    [Form]

--- App/client/main.js
+++ App/client/main.js
@@ -7,3 +7,18 @@ Template.tchat.helpers(
     }
 );
 
+Template.form.events(
+    {
+        'submit form': function(event){
+            event.preventDefault();
+            var name = event.target.name.value;
+            var message = event.target.message.value;
+            Messages.insert(
+                {
+                    name: name,
+                    message: message
+                }
+            )
+        }
+    }
+);
