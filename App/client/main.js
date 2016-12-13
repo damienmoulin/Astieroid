9cde98a55d0dc13ccba6eba2a88987801838e6ac
damienmoulin <damien.moulin1@gmail.com>
2016-12-13 11:03:41 +0100

    [Form] Connexion

--- App/client/main.js
+++ App/client/main.js
@@ -1,24 +1,42 @@
-Template.tchat.helpers(
-    {
-        all_messages : function()
-        {
-            return Messages.find();
-        }
-    }
-);
+if(Meteor.isClient) {
+    Session.setDefault('page', 'connexion');
 
-Template.form.events(
-    {
-        'submit form': function(event){
-            event.preventDefault();
-            var name = event.target.name.value;
-            var message = event.target.message.value;
-            Messages.insert(
-                {
-                    name: name,
-                    message: message
-                }
-            )
+    Template.body.helpers({
+        currentPage: function (page) {
+            return Session.get('page')
         }
-    }
-);
+    });
+
+    Template.connexion.events(
+        {
+            'submit form': function(event) {
+                event.preventDefault();
+                name = event.target.name.value;
+                Session.set('page', 'tchat');
+            }
+        }
+    );
+
+    Template.tchat.helpers(
+        {
+            all_messages: function () {
+                return Messages.find();
+            }
+        }
+    );
+
+    Template.form.events(
+        {
+            'submit form': function (event) {
+                event.preventDefault();
+                var message = event.target.message.value;
+                Messages.insert(
+                    {
+                        name: name,
+                        message: message
+                    }
+                )
+            }
+        }
+    );
+}
